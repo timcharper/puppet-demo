fb318feb6bf74917ae305241aa64a0be30c7aa37
Tim Harper <timcharper@gmail.com>
2009-08-26 21:40:01 -0400

    so do commands (check to see if it has the word Hello)

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@ exec { "Create the file /tmp/hello_puppet":
   cwd     => "/tmp",
   command => "echo 'Hello Puppet' > hello_puppet",
   path    => "/bin",
-  unless  => "[ -f /tmp/hello_puppet ]"
+  unless  => "grep Hello /tmp/hello_puppet"
 }
 
