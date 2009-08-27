388c63661cfd435606863c980fc97e07db18e931
Tim Harper <timcharper@gmail.com>
2009-08-27 19:14:47 -0400

    goodbye, cruel world

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,11 +5,5 @@ Exec {
   path => "/bin:/usr/bin"
 }
 
-exec { "Create the file /tmp/hello_puppet":
-  cwd     => "/tmp",
-  command => $operatingsystem ? { Ubuntu => "echo 'Hello Ubuntu' > hello_puppet", default => "echo 'Hello, linux' > hello_puppet" },
-  unless  => "grep Hello /tmp/hello_puppet"
-}
-
 include general
 include passenger
