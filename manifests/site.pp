ff73c985e0520cf48140479ca819d9858354f062
Tim Harper <timcharper@gmail.com>
2009-08-26 21:42:34 -0400

    using operatingsystem facter to change the message created

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 exec { "Create the file /tmp/hello_puppet":
   cwd     => "/tmp",
-  command => "echo 'Hello Puppet' > hello_puppet",
+  command => $operatingsystem ? { Ubuntu => "echo 'Hello Ubuntu' > hello_puppet", default => "echo 'Hello, linux' > hello_puppet" },
   path    => "/bin",
   unless  => "grep Hello /tmp/hello_puppet"
 }
