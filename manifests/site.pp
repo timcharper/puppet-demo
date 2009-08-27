b41f4609ac2de186ad5884a4a474e3bc59099918
Tim Harper <timcharper@gmail.com>
2009-08-26 21:39:00 -0400

    shell conditions work

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,5 +2,6 @@ exec { "Create the file /tmp/hello_puppet":
   cwd     => "/tmp",
   command => "echo 'Hello Puppet' > hello_puppet",
   path    => "/bin",
+  unless  => "[ -f /tmp/hello_puppet ]"
 }
 
